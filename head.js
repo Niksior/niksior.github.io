3615ff47fe9acd9164d852470c304bcdabb44b60
Niksior <heroes.VS.misio@gmail.com>
2016-11-08 20:18:41 +0100

    test

--- head.js
+++ head.js
@@ -6,12 +6,15 @@ function yScroll(){
 	tB = document.getElementById('navbar');
 	
 	if(yPos){
-		tB.style.top = "0%";
+		//tB.style.top = "0%";
+		tB.style.position = "fixed";
 	}
 
 	else  {
-		tB.style.top = "10%";
+		//tB.style.top = "10%";
+		tB.style.position = "relative";
 	}
+	console.log(yPos);
 }
 
 window.addEventListener("scroll", yScroll);
